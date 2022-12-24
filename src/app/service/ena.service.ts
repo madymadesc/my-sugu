f0cb6f6c73673f2327c6222bacdac791daa73d71
madymadesc <madymadesc@gmail.com>
2022-12-24 23:36:50 +0000

    methode pour gere l'etat du menu

--- src/app/service/ena.service.ts
+++ src/app/service/ena.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnaService {
 
+  private isMenuEnabled = new Subject<boolean>();
   constructor() { }
+
+  // Création d'une méthode pour gérer l'état du menu latéral (activé ou désactivé)
+setMenuState(enabled){
+  this.isMenuEnabled.next(enabled);
+}
+
+// Méthode pour obtenir l'état du menu
+getMenuState(): Subject<boolean>{
+  return this.isMenuEnabled;
+}
 }
